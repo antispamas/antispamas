b1391646bcc427ec362a7bbd89824eec13fd9003
antispamas <kmnru@hi2.in>
2017-07-09 12:25:47 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -90,7 +90,7 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {283598958,331485190,416462361,406155702,434513279, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
